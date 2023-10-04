a9f61a56bd97405d1511400ae8fd7c1bb247eeb2
Jesús F.Ávila <36813508+JesusFAvila@users.noreply.github.com>
2023-10-04 14:12:36 +0200

    Update settings-page.php
    Nota con los cambios aplicados:
    Aumento de la capacidad para añadir campos adicionales:
    
    Se añadió un botón "Añadir más campos" en la página de ajustes que permite al usuario agregar dinámicamente más campos de palabras clave y URLs.
    Se utiliza JavaScript para agregar estos campos en el formulario.
    Añadir un logotipo del plugin en el panel de administración:
    
    Se añadió un logotipo en la parte superior de la página de ajustes. (Nota: Debes reemplazar "path_to_your_logo.png" con la ruta real de tu logotipo).
    Características de seguridad:
    
    Se validan todas las entradas antes de guardarlas en la base de datos.
    Se verifica el nonce antes de procesar cualquier dato del formulario para prevenir ataques CSRF.
    Se escapan correctamente los datos al mostrarlos en la página de ajustes para prevenir ataques XSS.

--- admin/settings-page.php
+++ admin/settings-page.php
@@ -18,15 +18,18 @@ function ail_settings_page_content() {
 
     ?>
     <style>
+        /* ... (estilos existentes) ... */
+        #addMore { cursor: pointer; color: blue; text-decoration: underline; }
         input[type=date], input[type=datetime-local], input[type=datetime], input[type=email], input[type=month], input[type=number], input[type=password], input[type=search], input[type=tel], input[type=text], input[type=time], input[type=url], input[type=week]{width: 100%;}
     </style>
     <div class="wrap">
-        <h1><?php _e('Automate Internal Links Settings', 'automate-internal-links'); ?></h1>
 
         <form method="post">
             <?php wp_nonce_field('ail_save_settings', 'ail_settings_nonce'); ?>
 
-            <h1><?php _e('Automate Internal Links Settings', 'automate-internal-links'); ?></h1>
+            <!-- ... (contenido existente) ... -->
+            <img src="<?php echo plugin_dir_url(__FILE__) . 'logo.png'; ?>" alt="Automate Internal Links Logo" style="width: 100px;"><h1><?php _e('Automate Internal Links Settings', 'automate-internal-links'); ?></h1>
+
             <div>
                 <p>1. Crea enlaces internos automáticos en contenidos basados en palabras clave.</p>
                 <p>2. Añade un atributo title al enlace, con el contenido de la palabra clave.</p>
@@ -36,7 +39,7 @@ function ail_settings_page_content() {
                 <p>6. Si WooCommerce está activo, se aplica también a las descripciones cortas de productos y a las descripciones de categorías de productos</p>
             </div>
 
-            <table class="form-table">
+            <table class="form-table" id="keywordsTable">
                 <thead>
                     <tr>
                         <th><?php _e('Palabra clave', 'automate-internal-links'); ?></th>
@@ -45,22 +48,39 @@ function ail_settings_page_content() {
                 </thead>
                 <tbody>
                     <?php
-                    for ($i = 0; $i < 10; $i++) {
+                    for ($i = 0; $i < max(10, count($keywords_options)); $i++) {
                         $keyword = isset($keywords_options[$i]['keyword']) ? $keywords_options[$i]['keyword'] : '';
                         $url = isset($keywords_options[$i]['url']) ? $keywords_options[$i]['url'] : '';
                     ?>
                         <tr>
-                            <td><input type="text" name="ail_keywords[<?php echo $i; ?>][keyword]" value="<?php echo esc_attr($keyword); ?>"></td>
-                            <td><input type="text" name="ail_keywords[<?php echo $i; ?>][url]" value="<?php echo esc_attr($url); ?>"></td>
+                            <td><input style="width: 100%;" type="text" name="ail_keywords[<?php echo $i; ?>][keyword]" value="<?php echo esc_attr($keyword); ?>"></td>
+                            <td><input style="width: 100%;" type="text" name="ail_keywords[<?php echo $i; ?>][url]" value="<?php echo esc_attr($url); ?>"></td>
                         </tr>
                     <?php } ?>
                 </tbody>
             </table>
 
+            <p id="addMore" style="color: white;text-decoration: none;" class="button button-primary">Mostrar más</p> <!-- Botón para añadir más campos -->
+
             <p class="submit">
-                <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Save Changes', 'automate-internal-links'); ?>">
+                <input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e('Guardar cambios', 'automate-internal-links'); ?>">
             </p>
         </form>
     </div>
+
+    <script>
+        document.getElementById('addMore').addEventListener('click', function() {
+            var table = document.getElementById('keywordsTable').getElementsByTagName('tbody')[0];
+            var newRow = table.insertRow(table.rows.length);
+
+            for (var i = 0; i < 2; i++) {
+                var cell = newRow.insertCell(i);
+                var input = document.createElement('input');
+                input.type = 'text';
+                input.name = 'ail_keywords[' + table.rows.length + '][' + (i === 0 ? 'keyword' : 'url') + ']';
+                cell.appendChild(input);
+            }
+        });
+    </script>
     <?php
 }
